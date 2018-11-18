22b8a792e4b6d4ff3e26867d7e392e2046a50088
Goran Jankovic <goran.jankovic@vivify.com>
2018-11-18 12:56:38 +0100

    installing manjaro

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'dfsfsd' },
+    { id: 13, name: 'fdsads' },
+    { id: 14, name: 'DFFgrgr' },
+    { id: 15, name: 'Mr. dsfsd' },
+    { id: 16, name: 'Mr. Ndsfddfdsfice' }
+];
