b5801bd550dc555a7a0f1ac611683763e81f3eec
Goran <goran.jankovic@vivifyideas.com>
2018-11-19 09:38:21 +0100

    finished Display a Heroes List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,6 +8,7 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
